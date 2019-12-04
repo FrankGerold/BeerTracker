d014182dbd00585b665a26f7e1780d0bd8a95130
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-04 09:34:07 -0500

    Keg files and Seed

--- app/controllers/kegs_controller.rb
+++ app/controllers/kegs_controller.rb
@@ -1,2 +1,52 @@
 class KegsController < ApplicationController
+
+    def new 
+        @keg = Keg.new 
+    end 
+
+    def index
+        @kegs = Keg.all
+    end
+
+    def show 
+        find_keg
+    end 
+
+    def create 
+        @keg = Keg.new(keg_params)
+        if @keg.save 
+            redirect_to keg_path(@keg)
+        else 
+            flash[:errors] = @keg.errors.full_messages
+            redirect_to new_keg_path(@keg)
+        end 
+    end 
+    
+    def edit
+        find_keg 
+    end 
+
+    def update 
+        find_keg
+        if @keg.update(keg_params)
+            redirect_to keg_path(@keg)
+        else 
+            flash[:errors] = @keg.errors.full_messages
+            redirect_to edit_keg_path(@keg)
+        end 
+    end 
+    
+    def destroy 
+        find_keg
+        @keg.destroy 
+        redirect_to kegs_path 
+    end 
+
+    private 
+    def find_keg 
+        @keg = Keg.find(params[:id])
+    end 
+    def keg_params 
+        params.require(:keg).permit(:name, :type)
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do 
+    User.create({
+        name: Faker::Movies::StarWars.character,
+        bio: Faker::Movies::StarWars.quote
+    })
+    Keg.create({
+        name: Faker::Movies::StarWars.call_sign, 
+        beer_type: Faker::Movies::StarWars.call_squadron
+    })
+    Floor.create({
+        building: Faker::Movies::StarWars.planet, 
+        floor_number: rand(1..10).to_s
+    })
+    
+    
