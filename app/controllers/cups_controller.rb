ea39b586c8de09ee9d50d63678178879da79d4ae
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-04 10:13:05 -0500

    Fixed some dependencies and attribute names changes

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,2 +1,14 @@
 class CupsController < ApplicationController
+    def new 
+        @cup = Cup.new 
+    end 
+    def create 
+        @cup = Cup.new(params.require(:cup).permit(:rating, :user_id, :keg_id))
+        if @cup.save 
+            redirect_to user_path(@cup.user)
+        else 
+            flash[:errors] = @cup.errors.full_messages
+            redirect_to new_cup_path(@cup)
+        end 
+    end 
 end
--- app/models/keg.rb
+++ app/models/keg.rb
@@ -1,4 +1,7 @@
 class Keg < ApplicationRecord
-  has_many :cups
+  has_many :cups, dependent: :destroy
   has_many :users, through: :cups
+  has_many :keg_floors, dependent: :destroy
+  has_many :floors, through: :keg_floors 
+  validates :name, presence: true 
 end
