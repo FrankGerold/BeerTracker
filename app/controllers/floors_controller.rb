400225f011338d98f7ae998cbd11389d328bd006
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-04 10:47:11 -0500

    Floor index and show pages, fixed keg route typos

--- app/controllers/floors_controller.rb
+++ app/controllers/floors_controller.rb
@@ -1,2 +1,17 @@
 class FloorsController < ApplicationController
+
+  def index
+    @floors = Floor.all
+  end
+
+  def show
+    find_floor
+  end
+
+  private
+
+  def find_floor
+    @floor = Floor.find(params[:id])
+  end
+
 end
