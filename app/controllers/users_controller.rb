d440a7ec361e10cf093b30e35455758ee40a713c
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-05 14:41:52 -0500

    hella stuff I don't even know

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id 
       redirect_to @user
     else
       flash[:errors] = @user.errors.full_messages
