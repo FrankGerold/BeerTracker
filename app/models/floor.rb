6c1089e045b83e56449cde5bf88b3dc7b2e94c25
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-05 16:49:06 -0500

    Floor most pop beer

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,6 +1,12 @@
 class Floor < ApplicationRecord
   has_many :keg_floors, dependent: :destroy
   has_many :kegs, through: :keg_floors
-  validates :building, presence: true 
-  validates :floor_number, presence: true, inclusion: 1..10 
+  validates :building, presence: true
+  validates :floor_number, presence: true, inclusion: 1..10
+
+  def most_popular_beer
+    kegs = Cup.all.map{|cup| cup.keg}
+    kegs.max_by{|keg| kegs.count(keg)}
+  end
+
 end
