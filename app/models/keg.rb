085dda61a8514bdc4f06ba128785beb9d6cee38d
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-05 18:07:03 -0500

    part of kegfloor newest

--- app/models/keg.rb
+++ app/models/keg.rb
@@ -2,33 +2,35 @@ class Keg < ApplicationRecord
   has_many :cups, dependent: :destroy
   has_many :users, through: :cups
   has_many :keg_floors, dependent: :destroy
-  has_many :floors, through: :keg_floors 
-  validates :name, presence: true 
+  has_many :floors, through: :keg_floors
+  validates :name, presence: true
 
   def name_and_type
     "#{self.name} - #{self.beer_type}"
-  end 
+  end
 
-  def average 
+  def average
     if self.cups.length > 0
-      @ratings = self.cups.map do |cup| 
-         cup.rating 
+      @ratings = self.cups.map do |cup|
+         cup.rating
       end
       @average = (@ratings.sum / @ratings.size.to_f)
-      @average.round 
+      @average.round
     else
       return 0
-    end 
+    end
   end
-  
+
   def beer_emoji
     @string = ""
-    self.average.times do 
+    self.average.times do
       @string += "🍺"
-    end 
+    end
     @string
-  end 
+  end
+
   def show_day
     self.created_at.strftime("%A, %B %e")
-  end 
-end 
+  end
+  
+end
--- app/models/keg_floor.rb
+++ app/models/keg_floor.rb
@@ -2,7 +2,17 @@ class KegFloor < ApplicationRecord
   belongs_to :user
   belongs_to :keg
   belongs_to :floor
-  validates :user_id, presence: true 
-  validates :keg_id, presence: true 
-  validates :floor_id, presence: true 
+  validates :user_id, presence: true
+  validates :keg_id, presence: true
+  validates :floor_id, presence: true
+
+  def show_day
+    self.created_at.strftime("%A, %B %e")
+  end
+
+  def newest_update
+    my_kegs = KegFloor.all.select{|kf| kf.keg_id == self.keg_id }
+    sorted = my_kegs.all.sort{|a, b| a.created_at.strftime("%A, %B %e") <=> b.created_at.strftime("%A, %B %e")}
+    sorted.first
+  end
 end
