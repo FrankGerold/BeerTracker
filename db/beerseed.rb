741225528c9a40a452b4254498eab6625c0c2e84
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-06 10:07:42 -0500

    beer info

---
+++ db/beerseed.rb
@@ -0,0 +1,46 @@
+mos = Keg.create (
+  name: "Mosaic Session IPA",
+  beer_type: "India Pale Ale",
+  abv: 5.5,
+  ibu: 45,
+  desc: "Before the Mosaic hop even had a name (back when it was just known as “HBC 369”), our brewers couldn’t wait to get their hands on some. Mosaic Session IPA is bursting with vibrant grapefruit aroma and flavors of citrus and tropical fruit. Our signature dry-hopping technique delivers an incredibly drinkable lower ABV beer without sacrificing hop appeal. From a small test batch to the bottle you now hold in your hands, this isn’t a one-and-done beer. Sit back, have a few, and enjoy your session.",
+  photo_url: "https://www.karlstrauss.com/wp-content/uploads/2017/01/KS-BeerDetail-2017-01-MOS.png"
+)
+
+pum = Keg.create (
+  name: "Shipyard Pumpkinhead Ale",
+  beer_type: "Wheat Ale",
+  abv: 4.5,
+  ibu: 18,
+  desc: "Pumpkinhead is a crisp, refreshing wheat ale with delightful aromatics and subtle spice flavor.
+
+  To celebrate the fall season, we enhance the pumpkin flavor with a proprietary blend of spices including cinnamon and nutmeg.",
+  photo_url: "https://shipyard.com/wp-content/uploads/2018/02/pumpkinheadwcan-1.png"
+)
+
+gos = Keg.create (
+  name: "Goose IPA",
+  beer_type: "India Pale Ale",
+  abv: 5.9,
+  ibu: 55,
+  desc: "Goose Island’s flagship IPA is a six-time medal winner at the Great American Beer Festival. We’ve taken the traditional English Style and created our own fuller flavored IPA with bright citrus aromas and a bold hop finish. With hoppy, bold, and smooth flavor, Goose IPA is the perfect beer for hopheads and discovery drinkers alike.",
+  photo_url: "https://www.gooseisland.com/assets/images/beers/goose-ipa/IPA_HERO_CAN.png"
+)
+
+brok = Keg.create (
+  name: "Brooklyn Lager",
+  beer_type: "American Amber Lager"
+  abv: 5.2,
+  ibu: 33,
+  desc: "Our award-winning flagship stands tall as an instigator of the craft beer revolution. Brooklyn Lager debuted at a time when American lagers were the laughingstock of the beer world, and proved that American brewing was a force to be reckoned with. If you're looking for the dry-hopped amber lager that changed the world, look no further.",
+  photo_url: "https://brooklynbrewery.com/system/beers/1_shelf_lager-12oz-Beer-Page_original.png?1537982456"
+)
+
+dos = Keg.create (
+  name: "Dos Equis Lager Especial",
+  beer_type: "American Adjunct Lager / Pilsner",
+  abv: 4.2,
+  ibu: 10,
+  desc: "Dos Equis Lager Especial is a golden pilsner-style beer made from pure spring water and the choicest hops. With a balanced composition and a smooth, clean finish, it’s the party guest who is always invited and never overstays his welcome."
+  photo_url: "https://dosequis.com/wp-content/uploads/2019/01/lager-bottle.png"
+)
